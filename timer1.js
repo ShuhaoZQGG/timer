4736af2085601ab3245f3b46d48ec48504992945
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-21 17:49:12 -0400

    finish timer.js

---
+++ timer1.js
@@ -0,0 +1,11 @@
+let args = process.argv;
+args = args.slice(2);
+const timer = function(args){
+  for (const el of args){
+    if (Number(el) >= 0) {
+      setTimeout (() => process.stdout.write('\x07'), el* 2000);
+    }
+  }
+}
+
+timer(args);
